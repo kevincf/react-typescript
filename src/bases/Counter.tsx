57395d31884f69445ff13cefb635ee7a0b5e0b03
Kevin Cosio <krcs1801gmail.com>
2022-07-22 15:41:40 -0700

    first commit

--- src/bases/Counter.tsx
+++ src/bases/Counter.tsx
@@ -5,16 +5,25 @@ interface Props {
 }
 
 export const Counter = ({ initialValue = 0 }: Props) => {
-    const [counter, setCounter] = useState(initialValue)
+    const [counter, setCounter] = useState({
+        counter: initialValue,
+        clicks: 0
+    })
 
-    const handleClick = () => {
-        setCounter(prev => prev + 1)
+    const handleClick = (increment: number) => {
+        setCounter({
+            counter: counter.counter += increment
+            clicks: counter.clicks += 1,
+        })
     }
     return (
         <>
             <h1>Counter: {counter}</h1>
-            <button onClick={handleClick}>
+            <button onClick={() => handleClick(1)}>
                 +1
+            </button>
+            <button onClick={() => handleClick(5)}>
+                +5
             </button>
         </>
     )
